3306aa6cd8f612984192dd0556592b5d51622058
BorbaBruno <brunodeborba@outlook.com>
2022-10-15 07:24:57 -0300

    SignInButton

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,6 @@
 import styles from './styles.module.scss';
 import Image from 'next/image';
+import { SignInButton } from '../SignInButton';
 
 
 export function Header() {
@@ -11,6 +12,8 @@ export function Header() {
                     <a className={styles.active} href="#">Home</a>
                     <a href="#">Posts</a>
                 </nav>
+
+                <SignInButton />
             </div>
         </header>
     );
