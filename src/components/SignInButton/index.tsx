5dc53b15af2d7e7a6fe140aaf52b15917a568f32
BorbaBruno <brunodeborba@outlook.com>
2022-10-21 08:03:14 -0300

    finish auth

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,7 +1,7 @@
 import {FaGithub} from 'react-icons/fa';
 import styles from './styles.module.scss';
 import {FiX} from 'react-icons/fi';
-import {signIn, useSession} from 'next-auth/react';
+import {signIn, signOut, useSession} from 'next-auth/react';
 
 export function SignInButton() {
 
@@ -12,9 +12,10 @@ export function SignInButton() {
         <button 
         type="button"
         className={styles.signInButton}
+        onClick={() => signOut()}
     >
         <FaGithub color='#04d361' />
-        Bruno Borba
+        {session.user.name}
         <FiX color='#737380' className={styles.closeIcon}/>
     </button>
     ) : (
