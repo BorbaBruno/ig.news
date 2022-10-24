f0c1ba1c004d29ee20597a9076f2470d4a45d3c3
BorbaBruno <brunodeborba@outlook.com>
2022-10-24 14:31:50 -0300

    authentication resolve

--- src/services/fauna.ts
+++ src/services/fauna.ts
@@ -1,6 +1,6 @@
 import { Client } from 'faunadb';
 
 export const fauna = new Client({
-    secret: process.env.FAUNABD_KEY,
+    secret: process.env.FAUNADB_KEY,
     domain: 'db.us.fauna.com',
 })  
